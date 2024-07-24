1beba188367c444960abddafe7bd296799725427
Abdul Samad <50093236+abdulsamad100@users.noreply.github.com>
2024-07-24 12:46:48 +0500

    Updated Styles and Animations

--- app.js
+++ app.js
@@ -1,56 +1,73 @@
 const cars = [
-    { src: "car1.webp", heading: "Car 1" },
-    { src: "car2.webp", heading: "Car 2" },
-    { src: "car3.jpg", heading: "Car 3" },
-    { src: "car4.jpg", heading: "Car 4" },
-    { src: "car5.avif", heading: "Car 5" }
-  ];
-  
-  const loader = document.querySelector("#loader");
-    
-  document.addEventListener("DOMContentLoaded", () => {
-    loader.style.display = "none";
-  });
-  
-  let currind = 0;
-  let mainImage = document.querySelector("#mainimg");
-  let nextImage = document.querySelector("#nextimg");
-  let heading = document.querySelector("#heading");
-  
-  function updateImage(index) {
-      mainImage.classList.add("fade-out");
-      nextImage.src = cars[index].src;
-      nextImage.classList.add("fade-in");
-    
-    setTimeout(() => {
-      mainImage.src = cars[index].src;
-      mainImage.classList.remove("fade-out");
-      nextImage.classList.remove("fade-in");
-      heading.textContent = cars[index].heading;
-    }, 700);
-  }
-  
-  mainImage.src = cars[currind].src;
-  heading.textContent = cars[currind].heading;
-  
-  const previous = document.querySelector("#previous");
-  const next = document.querySelector("#next");
-  
-  previous.addEventListener('click', () => {
-    if (currind > 0) {
-      currind--;
+  { src: "car1.avif", heading: "LAMBORGHINI" },
+  { src: "car2.jpg", heading: "FERARRI" },
+  { src: "car3.jpg", heading: "PORSCHE" },
+  { src: "car4.jpg", heading: "KOINEGSEGG" },
+  { src: "car5.jpg", heading: "BUGATTI" }
+];
+
+const loader = document.querySelector("#loader");
+
+let currind = 0;
+let mainImage = document.querySelector("#mainimg");
+let nextImage = document.querySelector("#nextimg");
+let heading = document.querySelector("#heading");
+let isFirstImageChange = true;
+
+function updateImage(index) {
+  mainImage.classList.add("fade-out");
+  nextImage.src = cars[index].src;
+  nextImage.classList.add("fade-in");
+  setTimeout(() => {
+    if (isFirstImageChange) {
+      detypeText(cars[index].heading);
     } else {
-      currind = cars.length - 1;
+      typeText(cars[index].heading);
     }
-    updateImage(currind);
-  });
-  
-  next.addEventListener('click', () => {
-    if (currind < cars.length - 1) {
-      currind++;
+    mainImage.src = cars[index].src;
+    mainImage.classList.remove("fade-out");
+    nextImage.classList.remove("fade-in");
+  }, 1000);
+}
+
+function typeText(text) {
+  heading.textContent = "";
+  let i = 0;
+  let interval = setInterval(() => {
+    if (i < text.length) {
+      heading.textContent += text[i];
+      i++;
     } else {
-      currind = 0;
+      clearInterval(interval);
     }
-    updateImage(currind);
-  });
-  
+  }, 50);
+}
+
+function detypeText(text) {
+  let i = text.length - 1;
+  let interval = setInterval(() => {
+    if (i >= 0) {
+      heading.textContent = heading.textContent.substring(0, i);
+      i--;
+    } else {
+      clearInterval(interval);
+      typeText(text);
+    }
+  }, 50);
+}
+
+mainImage.src = cars[currind].src;
+typeText(cars[currind].heading);
+
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+previous.addEventListener('click', () => {
+  currind = (currind > 0) ? currind - 1 : cars.length - 1;
+  updateImage(currind);
+});
+
+next.addEventListener('click', () => {
+  currind = (currind < cars.length - 1) ? currind + 1 : 0;
+  updateImage(currind);
+});
